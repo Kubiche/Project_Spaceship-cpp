bdd82007ba8d0fbc35c5bc892fda608961c16b95
Kubiche <juanquy25@gmail.com>
2023-03-01 12:28:00 -0500

    new parser

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -1,5 +1,8 @@
 #include "CommandParser.h"
 #include "Arduino.h"
+#include "MAX72XX.h"
+
+extern MAX72XX led;
 
 void parseCommand(uint8_t message[])
 {
@@ -11,8 +14,10 @@ void parseCommand(uint8_t message[])
     else 
     {
         uint8_t command_type = message[0];
-        uint8_t command_arg_1 = message[1];
-        uint8_t command_arg_2 = message[2];
+        uint8_t command_device = message[1];
+        uint8_t command_arg_1 = message[2];
+        uint8_t command_arg_2 = message[3];
+        
 
         switch(command_type)
         {
@@ -20,7 +25,16 @@ void parseCommand(uint8_t message[])
                 break;
 
             case 1:  // LED ON 
+                led.SetLed(command_device, command_arg_1, command_arg_2, true);
+                break;
                 
+            case 2:  // LED OFF 
+                led.SetLed(command_device, command_arg_1, command_arg_2, false);
+                break;
+                
+            case 3:  // Show in BAR
+                led.Show_in_bar(command_device, command_arg_1, command_arg_2);
+                break;
                 
                 
         } 
