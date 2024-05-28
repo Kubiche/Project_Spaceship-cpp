d4401d050f8e5da102185b283d4b52b4ed2bfe65
Kubiche <juanquy25@gmail.com>
2024-05-28 14:43:05 -0400

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@ This code is still under construction.
 */
 #include <Arduino.h>
 #include "IO.h"
+#include "debug.h"
 
 
 void setup() 
 { 
   initIO();
+  debugln("Setup Complete")
 }
 
 void loop() 
