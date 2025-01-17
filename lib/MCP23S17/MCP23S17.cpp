78de232b7b8a3869602913da85bedf6f4a90150b
Kubiche <juanquy25@gmail.com>
2025-01-17 11:12:28 -0500

    tbc

---
+++ lib/MCP23S17/MCP23S17.cpp
@@ -0,0 +1,14 @@
+#include "MCP23S17.h"
+#include <SPI.h>
+
+
+/**
+ * @brief Configures the device
+ * 
+ * @param CS Chip Select pin for the MCP23S17 ICs
+ * @param interrupt_pin Interrup pin from the MCP23S17 ICs
+ */
+void MCP23S17::begin(uint8_t CS, uint8_t interrupt_pin)
+{
+
+}
